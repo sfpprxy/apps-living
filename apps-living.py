31f83814ee122733b34116fc5091fb902712c3bd
Joe <rxyjoey@gmail.com>
2016-03-10 01:36:20 +0000

    Generate sample JSON API

--- apps-living.py
+++ apps-living.py
@@ -2,19 +2,29 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-log_id
-house_name
-student_name
-arrive_date
-collect_date
-confirm_state
+log_id = 6097248
+house_name = 'FH201'
+student_name = 'QIAORUI CUI'
+arrive_date = '2016-03-14 21:35:59'
+collect_date = '2016-03-16 08:59:22'
+confirm_state = 'null'
 
-example = [
+sample_data = [
     {
-
+        'logId': log_id,
+        'houseName': house_name,
+        'studentName': student_name,
+        'arriveDate': arrive_date,
+        'collectDate': collect_date,
+        'confirmState': confirm_state
     },
     {
-
+        'logId': log_id,
+        'houseName': house_name,
+        'studentName': student_name,
+        'arriveDate': arrive_date,
+        'collectDate': collect_date,
+        'confirmState': confirm_state
     }
 ]
 
@@ -24,9 +34,9 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/example', methods=['GET'])
+@app.route('/api/sample', methods=['GET'])
 def get_tasks():
-    return jsonify({'example': example})
+    return jsonify({'sampleData': sample_data})
 
 
 if __name__ == '__main__':
