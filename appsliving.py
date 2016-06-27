004d300fef46f55d79de5ed07c02faeaad982064
Joe <rxyjoey@gmail.com>
2016-06-27 23:14:20 +0100

    Renaming packer.py to service.py

--- appsliving.py
+++ appsliving.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify, request
 from flask.ext.cors import CORS
-import packer
+import service
 
 
 app = Flask(__name__, static_url_path='')
@@ -20,36 +20,36 @@ def root():
 
 @app.route('/api/sample', methods=['GET'])
 def get_sample():
-    return jsonify({'sampleData': packer.log_id})
+    return jsonify({'sampleData': service.log_id})
 
 
 @app.route('/api/tenants/<house_name>', methods=['GET'])
 def get_students(house_name):
-    return jsonify({'tenants': packer.get_tenants(house_name)})
+    return jsonify({'tenants': service.get_tenants(house_name)})
 
 
 @app.route('/api/houses', methods=['GET'])
 def get_houses():
-    return jsonify({'houses': packer.get_houses()})
+    return jsonify({'houses': service.get_houses()})
 
 
 @app.route('/api/rooms/<house_name>', methods=['GET'])
 def get_rooms(house_name):
-    return jsonify({'rooms': packer.get_rooms(house_name)})
+    return jsonify({'rooms': service.get_rooms(house_name)})
 
 
 @app.route('/api/room/<room_id>', methods=['GET'])
 def get_room(room_id):
-    return jsonify({'room': packer.get_room(room_id)})
+    return jsonify({'room': service.get_room(room_id)})
 
 
 # TODO: implement this post
 @app.route('/api/new-room', methods=['POST'])
 def post_add():
     if request.json:
-        data = request.json # will be
+        data = request.json
         print(data)
-        return 'OK to front'
+        return 'new-room OK!'
 
     else:
         print('no json received')
