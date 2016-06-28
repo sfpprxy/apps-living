6a8d3d79e346268f93df50ff01ed3cba1ddf16e8
Joe <rxyjoey@gmail.com>
2016-06-28 23:42:55 +0100

    Add logger for project

---
+++ appslogger.py
@@ -0,0 +1,14 @@
+import logging
+import logging.handlers
+
+LOG_FILE = 'log.log'
+
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1024 * 1024, backupCount=5)
+fmt = '[%(asctime)s] %(levelname)-8s - %(message)s (%(filename)s:%(lineno)s)'
+
+formatter = logging.Formatter(fmt)
+handler.setFormatter(formatter)
+
+logger = logging.getLogger('log.log')
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
