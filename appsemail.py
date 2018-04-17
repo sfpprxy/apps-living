dab3101a4dc63e9d3662171bf5d7a58226d2b614
sfpprxy <rxyjoey@gmail.com>
2018-04-17 11:36:46 +0800

    refactor: new email template

--- appsemail.py
+++ appsemail.py
@@ -12,13 +12,32 @@ def send(toaddr, name, code):
         msg = MIMEMultipart()
         msg['From'] = fromaddr
         msg['To'] = toaddr
-        msg['Subject'] = 'Your parcel is arrived'
+        msg['Subject'] = 'Your parcel has arrived at APPS LIVING.'
         body = ('Dear ' + name + ',\n\n'
                 'You have a parcel arrived at our office, please bring your ID or verification code to collect.' '\n\n'
                 'Verification Code: ' + code + '\n\n'
                 'Kind Regards,\n\n'
                 'APPS Living Service Team')
-        msg.attach(MIMEText(body, 'plain'))
+        new_body = """
+        Dear {name},
+        
+        Your parcel has been delivered to our main office. To collect your parcel please do the
+        following:
+        - Bring your Student ID
+        - Show reception your verification code
+        
+        Verification Code : {code}
+        
+        Please note : If you would like someone else to collect your parcel, please send a verification
+        message to a member of staff.
+        
+        Kind Regards,
+        
+        Student Service Team
+        APPS LIVING
+        """.format(name=name, code=code)
+
+        msg.attach(MIMEText(new_body, 'plain'))
 
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.starttls()
