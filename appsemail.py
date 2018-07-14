5ded0396a9bf64237812452c70bd98bc0a62f054
sfpprxy <rxyjoey@gmail.com>
2018-07-14 21:49:52 +0800

    refactor: new email template 20180714 02

--- appsemail.py
+++ appsemail.py
@@ -13,31 +13,31 @@ def send(toaddr, name, code):
         msg['From'] = fromaddr
         msg['To'] = toaddr
         msg['Subject'] = 'Your parcel has arrived at APPS LIVING.'
+        # indent due to python multi-line string
         body = """
-        Dear {name},
-        
-        Your parcel has been received by APPS LIVING. To collect your parcel please do the following:
+Dear {name},
 
-        - Bring your Student ID
-        - Show your verification code to the member of staff
-        
-        Verification Code : {code}
-        
-        For students staying at Manor Park, Acacia House, Park Road or Falcon House, please collect* from the Manor Park Office which is open Monday-Friday 2pm-4pm.
+Your parcel has been received by APPS LIVING. To collect your parcel please do the following:
 
-        *If you receive this email before 2pm, your parcel will be delivered to the Manor Park Office on the same working day.
-        *If you receive this email after 2pm, your parcel will be delivered to the Manor Park Office on the next working day.
+- Bring your Student ID
+- Show your verification code to the member of staff
 
-        For students staying at the rest of properties of APPS LIVING please collect* from the Main Office. 
-        
-        Please note: If you would like someone else to collect your parcel, please send a verification message 
-        to a member of staff via WeChat: APPSLIVINGHELP
+Verification Code : {code}
 
-        
-        Kind Regards,
-        
-        APPS LIVING
+For students staying at Manor Park, Acacia House, Park Road or Falcon House, please collect* from the Manor Park Office which is open Monday-Friday 2pm-4pm.
+
+*If you receive this email before 2pm, your parcel will be delivered to the Manor Park Office on the same working day.
+*If you receive this email after 2pm, your parcel will be delivered to the Manor Park Office on the next working day.
+
+For students staying at the rest of properties of APPS LIVING please collect* from the Main Office. 
+
+Please note: If you would like someone else to collect your parcel, please send a verification message to a member of staff via WeChat: APPSLIVINGHELP
+
+Kind Regards,
+
+APPS LIVING
         """.format(name=name, code=code)
+        print(body)
 
         msg.attach(MIMEText(body, 'plain'))
 
@@ -51,3 +51,4 @@ def send(toaddr, name, code):
     except smtplib.SMTPRecipientsRefused:
         return False
 
+# send('mandy.chen@apps-living.co.uk', 'Cui', 1234)
